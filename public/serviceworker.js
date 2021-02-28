53e5592c5262751b068a734d9342307bc05e58de
Markos Rafael <escalante.markos18@gmail.com>
2021-02-27 22:02:44 -0800

    create install event listener.

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -11,3 +11,12 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
