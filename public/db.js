1502657831937da7798fc245b587e2a2df20cb18
Markos Rafael <escalante.markos18@gmail.com>
2021-02-27 22:20:04 -0800

    create our connection to indexdb and connect heroku port to server.js.

---
+++ public/db.js
@@ -0,0 +1,21 @@
+let db; 
+
+const request = window.indexedDB.open("budgettracker", 1);
+
+request.onupgradeneeded = event => {
+  const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+    
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log("Oh no! " + event.target.errorCode);
+};
