4403077ed75c7d6b92f2142fe0cb3be874dcf6e1
Markos Rafael <escalante.markos18@gmail.com>
2021-02-27 22:33:07 -0800

    check to see if the app is back online.

--- public/db.js
+++ public/db.js
@@ -26,3 +26,32 @@ const saveRecord = (record) => {
 
     store.add(record);
 }
+
+const checkDatabase = () => {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+
+        store.clear();
+      });
+    }
+  };
+}
+
+// listening for the app to come back online
+window.addEventListener("online", checkDatabase);
